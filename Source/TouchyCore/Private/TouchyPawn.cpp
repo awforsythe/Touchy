9043efbdb871e59ef440a272e0715f19db12e81f
Alex Forsythe <awforsythe@gmail.com>
2020-10-26 14:31:48 -0500

    Add Interactible, an interface
    An interface can't carry any data with it (so we can't expose properties
    for configuration or maintain state), but it allows multiple disparate
    classes to support the same set of functions. This gives us the same
    functionality as AInteractibleActor, but without requiring that all our
    interactible objects share a common base class.

--- Source/TouchyCore/Private/TouchyPawn.cpp
+++ Source/TouchyCore/Private/TouchyPawn.cpp
@@ -4,6 +4,7 @@
 #include "TouchyTypes.h"
 #include "InteractionTrace.h"
 #include "InteractibleActor.h"
+#include "Interactible.h"
 
 ATouchyPawn::ATouchyPawn(const FObjectInitializer& ObjectInitializer)
 	: Super(ObjectInitializer.DoNotCreateDefaultSubobject(ADefaultPawn::MeshComponentName))
@@ -37,9 +38,20 @@ void ATouchyPawn::OnUsePressed()
 {
 	// Check the InteractionTrace to find the actor in the center of our view (if any)
 	AActor* Actor = (InteractionTrace && InteractionTrace->Hit.bBlockingHit) ? InteractionTrace->Hit.Actor.Get() : nullptr;
+
+	// Here we demonstrate multiple ways of implementing a simple interaction system;
+	// in an actual game project you'd usually just want to pick one.
+
+	// 1. Inheritance via base class (AInteractibleActor)
 	AInteractibleActor* InteractibleActor = Cast<AInteractibleActor>(Actor);
 	if (InteractibleActor)
 	{
 		InteractibleActor->Used(this);
+	}
+
+	// 2. Inheritance via interface (IInteractible)
+	if (Actor && Actor->Implements<UInteractible>())
+	{
+		IInteractible::Execute_Used(Actor, this);
 	}
 }
---
+++ Source/TouchyCore/Public/Interactible.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+
+#include "Interactible.generated.h"
+
+/** Interface used by objects that can respond when the player looks at them
+	and presses the 'Use' button. Objects that implement the Interactible
+	interface should also block the 'Interactible' trace channel.
+*/
+UINTERFACE()
+class UInteractible : public UInterface
+{
+	GENERATED_BODY()
+};
+
+/** Native interface: C++ classes can extend IInteractible and override the
+	virtual Used_Implementation function; Blueprint classes will receive a Used
+	Blueprint event.
+*/
+class IInteractible
+{
+	GENERATED_BODY()
+
+public:
+	/** Called when a locally-controlled pawn looks at this object and presses the Use button. */
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category="Interaction")
+	void Used(class APawn* Pawn);
+
+	virtual void Used_Implementation(class APawn* Pawn) {};
+};
